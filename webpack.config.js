ddfd8af7d88ec0f4c35125ceb493bbe14400d7f7
md-maruf-hossain <sany.maruf@gmail.com>
2022-03-09 23:13:08 +0600

    created webpack and babel configuration

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const html = require('html-webpack-plugin')
+require('babel-polyfill')
+
+module.exports = {
+    mode: 'production',
+    entry: ['babel-polyfill', './src/index.jsx'],
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {   
+                test: /\.js/,
+                exclude: /node_module/,
+                use: [
+                {loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                        ],
+                        plugins: [
+                            "transform-class-properties",
+                            "transform-object-rest-spread"
+                        ]
+                    }
+                },
+            ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                {loader: 'style-loader'},
+                {loader: 'css-loader'}
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new html({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ],
+    resolve: {
+        fallback: { 
+            path: require.resolve("path-browserify"),
+            os: require.resolve("os-browserify/browser"),
+            https: require.resolve("https-browserify"),
+            http: require.resolve("stream-http"),
+            crypto: require.resolve("crypto-browserify"),
+            stream: require.resolve("stream-browserify")
+        },   
+    }
+}
