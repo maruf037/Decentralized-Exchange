041e942e293a28983be5257b1db5961bb1b95212
md-maruf-hossain <sany.maruf@gmail.com>
2022-03-12 00:23:49 +0600

    finished writhing the required functions name and documentation

--- contracts/DAX.sol
+++ contracts/DAX.sol
@@ -103,13 +103,51 @@ contract DAX {
     /// @dev It will revert is the if the user doesn't approve tokens beforehand to this contract.
     /// @param _token The token address
     /// @param _amount The quantity to deposit to the escrow contract.
-    function deposittokens(address _token, uint256 _amount) public {}
+    function depositTokens(address _token, uint256 _amount) public {}
 
     /// @notice To extract tokens.
     /// @param _token The token address to extract.
     /// @param _amount The amount of tokens to transfer.
     function extractTokens(address _token, uint256 _amount) public {}
 
-    
+    /* Add the market and limit the order functions with the parameters required for them to
+    work properly, since these will be the main functions used to create orders and to interact
+    with the DAX */
+    /// @notice To create a market order by filling one or more existing limit orders at the most profitable price given a token pair,
+    /// type of order (buy or sell) and the amount of tokens to trade, the _quantity is how many _firstSymbol tokens you want to buy if
+    /// it's a buy order or how many _firstSymbol tokens you want to sell at market price.
+    /// @param _type The type of order either 'buy' or 'sell'.
+    /// @param _firstSymbol The first token to buy or sell.
+    /// @param _secondSymbol The second token to create a pair.
+    /// @param _quantity The amount of tokens to sell or buy.
+    function marketOrder(bytes32 _type, bytes32 _firstSymbol, bytes32 _secondSymbol, uint256 _quantity) public {}
+
+    /// @notice To create a market order given a token pair, type of order, amount of tokens to trade and the price per token. If
+    /// the type is buy, the price will determine how many _secondSymbol tokens you are willing to pay for each _firstSymbol up until
+    /// your _quantity or better if there are more profitable prices. If the type if sell, the price will determine how many _secondSymbol
+    /// tokens you get for each _firstSymbol.
+    /// @param _type The type of order either 'buy' or 'sell'.
+    /// @param _firstSymbol The first symbol to deal with.
+    /// @param _secondSymbol The second symbol that you want to deal
+    /// @param _quantity How many tokens you want to deal, these are _firstSymbol tokens.
+    /// @param _pricePerToken How many tokens you get or pay for your other symbol, the total price is _pricePerToken * _quantity.
+    function limitOrder(bytes32 _type, bytes32 _firstSymbol, bytes32 _secondSymbol, uint256 _quantity, uint256 _pricePerToken) public {}
+        
+    /* Finally, add the view functions that you'll use as helpers and getters for important variables that your interface may need. */
+    /// @notice sorts the selected array of orders by price from lower to higher if it's a buy order or from highest to lowest if it's a sell order.
+    /// @param _type The type of order either 'buy' or 'sell'.
+    /// @return uint256[] Returns the sorted ids.
+    function sortIdsByPrices(bytes32 _type) public view returns (uint256[] memory) {}
+
+    /// @notice Checks if a pair is valid.
+    /// @param _firstSymbol The first symbol of the pair.
+    /// @param _secondSymbol The second symbol of the pair.
+    /// @return bool if the pair is valid or not.
+    function checkValidPair(bytes32 _firstSymbol, bytes32 _secondSymbol) public view returns(bool) {}
+
+    /// @notice Returns the token pairs
+    /// @param _token To get the array of token pair for that selected token.
+    /// @return bytes32[] An array containing the pairs.
+    function getTokenPairs(bytes32 _token) public view returns(bytes32[]) {}
 }   
 
