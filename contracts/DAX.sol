1ec4f3dc0799d94d841371b300d2e5205a73bb5e
md-maruf-hossain <sany.maruf@gmail.com>
2022-03-13 13:38:56 +0600

    Completed limit order function

--- contracts/DAX.sol
+++ contracts/DAX.sol
@@ -160,7 +160,7 @@ contract DAX {
     inside the Escrow contract to facilitate token management. */
     function extractTokens(address _token, uint256 _amount) public {
         /* The extract function is simply running the transferTokens() function to the owner's address, as long as they have some previous balance 
-        inside. Otherwise it will revert. */
+        inside. Otherwise it will revert.*/
         require(_token != address(0), 'Token address cannot be 0. You must specify the token address to extract');
         require(_amount > 0, 'Amount to extract must be greater than 0. You must send some tokens with this extract function.');
         Escrow(escrowByUserAddress[msg.sender]).transferTokens(_token, msg.sender, _amount);
@@ -187,7 +187,55 @@ contract DAX {
     /// @param _secondSymbol The second symbol that you want to deal
     /// @param _quantity How many tokens you want to deal, these are _firstSymbol tokens.
     /// @param _pricePerToken How many tokens you get or pay for your other symbol, the total price is _pricePerToken * _quantity.
-    function limitOrder(bytes32 _type, bytes32 _firstSymbol, bytes32 _secondSymbol, uint256 _quantity, uint256 _pricePerToken) public {}
+    function limitOrder(bytes32 _type, bytes32 _firstSymbol, bytes32 _secondSymbol, uint256 _quantity, uint256 _pricePerToken) public {
+        address userEscrow = escrowByUserAddress[msg.sender];
+        address firstSymbolAddress = tokenAddressBySymbol[_firstSymbol];
+        address secondSymbolAddress = tokenAddressBySymbol[_secondSymbol];
+
+        require(firstSymbolAddress != address(0), 'First symbol address cannot be 0. The first symbol has not bee whitelisted.');
+        require(secondSymbolAddress != address(0), 'Second symbol address cannot be 0. The second symbol has not bee whitelisted.');
+        require(isTokenSymbolWhitelisted[_firstSymbok]. 'The first symbol must be whitelisted to trade with it.');
+        require(isTokenSymbolWhitelisted[_secondSymbol]. 'The second symbol must be whitelisted to trade with it.');
+        require(userEscrow != address(0), 'You must deposit some tokens before creating orders, use depositToken().');
+        require(checkValidPair(_firstSymbol, _secondSymbol), 'The pair must be a valid pair.');
+
+        // After that, execute the buy functionality if the user is creating a buy limit order.
+        Order memory myOrder = Order(orderIdCounter, msg.sender, _type, _firstSymbol, _secondSymbol, _quantity, _pricePerToken, now, OrderState.OPEN);
+        orderByID[orderIdCounter] = myOrder;
+        if(_type == 'buy') {
+            // Check that the user has enough of the second symbol if he wants to buy the first symbol at that price.
+            require(IERC20(secondSymbolAddress).balanceOf(userEscrow) => _quantity, 'You must have enough second token funds in your escrow contract to create this buy order.');
+            buyOrders.push(myOrder);
+
+            // Sort existing orders by price the most efficent way possible, we could optimize even more by creating a buy array for each token.
+            uint256[] memory sortedIds = sortIdsByPrice('buy');
+            delete buyOrders;
+            buyOrders.length = sortedIds.length;
+            for(uint256 i = 0; i < sortedIds.length; i++) {
+                Order memory order = orderByID[sortedIds[i]];
+                if(order.state == OrderState.OPEN) {
+                    buyOrders[i] = orderById[sortedIds[i]];
+                    buyOrderIndexById[sortedIds[i]] = i;
+                } else {
+                    // Check that the user has enough of the first symbol if he wonts to sell it for the second symbol.
+                    require(IERC20(firstSymbolAddress).balanceOf(userEscrow) => order.quantity, 'You must have enough first token funds in your escrow contract to create this buy order.');
+
+                    // Add the new order.
+                    sellOrders.push(myOrder);
+
+                    // Sort existing orders by price the most efficent way possible. we could optimize even more by creating a sell array for each token.
+                    uint256[] memory sortedIds = sortIdsByPrice('sell');
+                    delete sellOrders; //Reset orders.
+                    sellOrders.length = sortedIds.length;
+                    for(uint256 i = 0; i < sortedIds.length; i++) {
+                        sellOrders[i] = orderByID[sortedIds[i]];
+                        sellOrderIndexById[sortedIds[i]] = i;
+                    }
+                }
+                orderIdCounter++;
+            }
+        }
+    }
         
     /* Finally, add the view functions that you'll use as helpers and getters for important variables that your interface may need. */
     /// @notice sorts the selected array of orders by price from lower to higher if it's a buy order or from highest to lowest if it's a sell order.
